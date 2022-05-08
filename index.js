c85ea4a365cd9509395e414133fa6f3b05730bd6
ShihabReza <shihabreza457@gmail.com>
2022-05-08 12:07:26 +0600

    first commit

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ require('dotenv').config()
 const query = require('express/lib/middleware/query');
 const app = express()
 app.use(cors())
-const port = 5000
+const port = process.env.PORT || 5000
 app.use(express.json())
 
 
